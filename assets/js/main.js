307193ce5331b440dca4f1905cee2dfb4a60fd00
ALAABDESSAMED <93202870+ALAABDESSAMED@users.noreply.github.com>
2022-11-16 12:29:25 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+function loader(){
+    document.getElementById("preloader").style.opacity="0";
+}
+function loader2(){
+    document.getElementById("preloader").style.display="none";
+}
+setTimeout(loader,5000);
+setTimeout(loader2,5501);
